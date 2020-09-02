96473a1572daf9613fe5d504d93ffcc6442c255e
Tom Matz <tm774119@ohio.edu>
2020-09-02 17:42:04 -0400

    commit test

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Tom Matz
+ *        Date: September 2nd 2020
+ * Description: git lab 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
